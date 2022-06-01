f2959efb5dc1aa697f0e476d4d0752cbb59199b7
Kris Dunning <dunning.kris@gmail.com>
2022-06-01 13:03:28 -0700

    commit due to repull

--- js/app.js
+++ js/app.js
@@ -117,125 +117,101 @@ Workout.prototype.generateWorkoutMovements = function () {
 };
 
 // *********************************************
-  //     INSTANTIATION & LOCAL STORAGE PT 2
-  // *********************************************
-  // console.log(new Workout('stretch', '10'));
-  // console.log(new Workout('strength', '10')); 
+//     INSTANTIATION & LOCAL STORAGE PT 2
+// *********************************************
+// console.log(new Workout('stretch', '10'));
+// console.log(new Workout('strength', '10')); 
 
-  //****************************************
-  //            HELPER FUNCTIONS
-  //****************************************
-  
-  // RANDOM # GENERATOR
-  function randNum(max) {
-    return Math.floor(Math.random() * max);
-  }
-  
-  
-  // *********************************************
-  //              CHART RENDERING
-  // *********************************************
+//****************************************
+//            HELPER FUNCTIONS
+//****************************************
+
+// RANDOM # GENERATOR
+function randNum(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// *********************************************
+//              CHART RENDERING
+// *********************************************
 
 
-  //********************************************
-  //            EVENT HANDLERS
-  //*******************************************
-  // CLICK HANDLER FOR --TYPE-- SELECTION
-  function handleType(event) {
-    event.preventDefault();
-    
-    let woType = document.getElementsByName('radio');
-    for (let i = 0; i < woType.length; i++) {
-      if (woType[i].checked) {
-        console.log(woType[i].value);
-        chosenType = woType[i].value;
-        // STORE TYPE LOCALLY
-        
-        // STEP 1: STRINGIFY DATA
-        let stringifiedType = JSON.stringify(woType[i].value);
-        
-        // STEP 2: ADD TO LOCAL STORAGE
-        workoutObject = {
-          type : "chosenType"
-        };
+//********************************************
+//            EVENT HANDLERS
+//*******************************************
+// CLICK HANDLER FOR --TYPE-- SELECTION
+function handleType(event) {
+  event.preventDefault();
+  let woType = document.getElementsByName('radio');
+  for (let i = 0; i < woType.length; i++) {
+    if (woType[i].checked) {
+      console.log(woType[i].value);
+      chosenType = woType[i].value;
+      // STORE TYPE LOCALLY
+      // STEP 1: STRINGIFY DATA
+      let stringifiedType = JSON.stringify(woType[i].value);
+      // STEP 2: ADD TO LOCAL STORAGE
+      workoutObject = {
+        type : "chosenType"
+      };
 
-        let workoutObjectJSON = JSON.stringify(workoutObject);
+      let workoutObjectJSON = JSON.stringify(workoutObject);
 
-        localStorage.setItem('CurrentWO', workoutObjectJSON);
-        
-        // PROOF OF LIFE (STORED DATA)
-        let chosenType = localStorage.getItem('CurrentWO');
-        let chosenType2 = JSON.parse(text);
+      localStorage.setItem('CurrentWO', workoutObjectJSON);
+      // PROOF OF LIFE (STORED DATA)
+      let chosenType = localStorage.getItem('CurrentWO');
+      let chosenType2 = JSON.parse(text);
 
-        console.log(chosenType2);
-      }
+      console.log(chosenType2);
     }
-      
   }
-  
-  // CLICK HANDLER FOR --TIME-- SELECTION
-  function handleTime(event) {
-    event.preventDefault();
-    
-    let woTime = document.getElementsByName('radio');
-    for (let i = 0; i < woTime.length; i++) {
-      if (woTime[i].checked) {
-        console.log(woTime[i].value);
-
-        chosenType = localStorage.getItem('CurrentWO');
-
-        chosenTime = woTime[i].value;
-        console.log(chosenTime);
-        console.log(chosenType);
-
-        let currentWorkout = new Workout(localStorage.getItem('CurrentWO'), chosenTime);
-    
-        console.log('this is our current workout', currentWorkout);
-        
-        // STORE TIME LOCALLY
-    
-        // STEP 1: STRINGIFY TIME
-        let stringifiedTime = JSON.stringify(woTime[i].value);
-        
-        // STEP 2: ADD TO LOCAL STORAGE
-        localStorage.setItem('CurrentWOTIME', stringifiedTime);
-    
-        // PROOF OF LIFE (STORED DATA)
-        let storedWOTIME = localStorage.getItem('CurrentWOTIME');
-        console.log(storedWOTIME);
-        console.log(storedWO);
-      }
+}
+// CLICK HANDLER FOR --TIME-- SELECTION
+function handleTime(event) {
+  event.preventDefault();
+  let woTime = document.getElementsByName('radio');
+  for (let i = 0; i < woTime.length; i++) {
+    if (woTime[i].checked) {
+      console.log(woTime[i].value);
+      chosenType = localStorage.getItem('CurrentWO');
+      chosenTime = woTime[i].value;
+      console.log(chosenTime);
+      console.log(chosenType);
+      let currentWorkout = new Workout(localStorage.getItem('CurrentWO'), chosenTime);
+      console.log('this is our current workout', currentWorkout);
+      // STORE TIME LOCALLY
+      // STEP 1: STRINGIFY TIME
+      let stringifiedTime = JSON.stringify(woTime[i].value);
+      // STEP 2: ADD TO LOCAL STORAGE
+      localStorage.setItem('CurrentWOTIME', stringifiedTime);
+      // PROOF OF LIFE (STORED DATA)
+      let storedWOTIME = localStorage.getItem('CurrentWOTIME');
+      console.log(storedWOTIME);
+      console.log(storedWO);
     }
-    
   }
-  
-  //****************************************
-  //            EVENT LISTENERS
-  //**************************************** 
-  
-  // typeForm.addEventListener('submit', handleSubmit);
-  
-  window.onload = (event) => {
-    if (document.getElementById('type-page')) {
-      nextBtn.addEventListener('click', handleType);
+}
+//****************************************
+//            EVENT LISTENERS
+//****************************************
+// typeForm.addEventListener('submit', handleSubmit);
+window.onload = (event) => {
+  if (document.getElementById('type-page')) {
+    nextBtn.addEventListener('click', handleType);
 
-    } else if (document.getElementById('time-page')) {
+  } else if (document.getElementById('time-page')) {
 
-      genBtn.addEventListener('click', handleTime);
-    }
-  };
-  
+    genBtn.addEventListener('click', handleTime);
+  }
+};
 
-  
-  // nextBtn.removeEventListener('click', handleType);
-  
-  /* **********************************************
+// nextBtn.removeEventListener('click', handleType);
+/* **********************************************
   LOCAL STORAGE
   ********************************************** */
- 
- // STEP 1: STRINGIFY DATA
- // let stringifiedProducts = JSON.stringify(allProductsArr);
- 
- // STEP 2: ADD TO LOCAL STORAGE
- // localStorage.setItem('products', stringifiedProducts);
+
+// STEP 1: STRINGIFY DATA
+// let stringifiedProducts = JSON.stringify(allProductsArr);
+// STEP 2: ADD TO LOCAL STORAGE
+// localStorage.setItem('products', stringifiedProducts);
 
